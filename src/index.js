0baa44415b3c45d1fd3d66c586c01bf60c0e5aec
Andrés Martínez Montes <amartinezm@efor.es>
2024-02-03 13:39:43 +0100

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
 window.onload = () => {
     console.log('loaded')
 
-    // let cells = <GET ALL cell ELEMENTS>
-    let cell = document.getElementsByClassName('cell-x')[0]
-    // for (const cell of cells) {
+    // let cells = <GET ALL cell ELEMENTS> - ya está
+    let cells = document.getElementsByClassName('cell')
+    let turnX = false
+     for (const cell of cells) {
         cell.onclick = (event) => {
-            const [, x, y] = event.target.id.split('-')
-            console.log(`click on ${x}:${y}`)
-            if (cell.className === 'cell')
-                cell.className = 'cell-o'
-            else {
-                if (cell.className === 'cell-o')
-                    cell.className = 'cell-x'
-                else
+            let listCells = document.getElementsByClassName('cell')
+            console.log(listCells)
+            if (listCells.length != 0){
+                const [, x, y] = event.target.id.split('-')
+                console.log(`click on ${x}:${y}`)
+
+                if (cell.className === 'cell' && turnX == false){
                     cell.className = 'cell-o'
+                    turnX = true
+                }
+
+                else if (cell.className === 'cell' && turnX == true) {
+                    cell.className = 'cell-x'
+                    turnX = false
+                }
+            } else {
+                alert("SE ACABO")
             }
+            
         }
-    // }
+    
+    }
+    
+}
+
+
+function resetTable(){
+    let cells = document.querySelectorAll("[class *='cell']")
+    for (const cell of cells) {
+        cell.className = 'cell'
+    }
+
+
+
+
 }
