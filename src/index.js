016a5cf70e7d64bc5c3302911accb6a8c7beb4ad
Andrés Martínez Montes <amartinezm@efor.es>
2024-02-03 13:59:39 +0100

    Finish match

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ window.onload = () => {
     let turnX = false
      for (const cell of cells) {
         cell.onclick = (event) => {
-            let listCells = document.getElementsByClassName('cell')
-            console.log(listCells)
-            if (listCells.length != 0){
+            if (!finish()){
                 const [, x, y] = event.target.id.split('-')
                 console.log(`click on ${x}:${y}`)
 
@@ -24,21 +22,35 @@ window.onload = () => {
             } else {
                 alert("SE ACABO")
             }
-            
         }
-    
     }
-    
 }
 
 
 function resetTable(){
-    let cells = document.querySelectorAll("[class *='cell']")
+    let cells = document.querySelectorAll("[class*='cell']")
     for (const cell of cells) {
         cell.className = 'cell'
+    }
+}
+
+function youWin(){
+    let cells = document.querySelectorAll("[class*='cell']")
+
+    for (i=0; i<cells.length; i++){
+
+
     }
 
 
 
+    return false
+}
 
+function finish(){
+    let listCells = document.getElementsByClassName('cell')
+    if (listCells.length == 0){
+        return true
+    }
+    return false
 }
